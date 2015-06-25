a6e2151896ec55beb7d6aacead29eead4f0bc8b4
john young & steven francisco <student@epicodus.com>
2015-06-25 11:32:10 -0700

    palindrome app working

---
+++ js/scripts.js
@@ -0,0 +1,29 @@
+var palindrome = function(word) {
+var x = word;
+var n = x.split("").reverse().join('');
+
+  if (x === n) {
+    return true;
+  }
+  else {
+    return false;
+  }
+};
+
+
+$(document).ready(function(){
+  $("form#palindrome").submit(function(event){
+    var user_input = $("input#user_word").val();
+    var palind = palindrome(user_input);
+
+    if(palind === true) {
+      $(".not").hide();
+      $("#result").show();
+    } else {
+      $("#result").hide();
+      $(".not").show();
+    }
+    event.preventDefault();
+  });
+
+});
